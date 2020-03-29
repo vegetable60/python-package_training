0ebfc77df058f3bb376ef7ea2878dba03ff76764
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-03-29 09:09:06 +0300

    update packages, clean tests, fix linter issues

--- hexlet_python_package/user.py
+++ hexlet_python_package/user.py
@@ -7,10 +7,21 @@ class User(object):
     """Object representation of user."""
 
     def __init__(self, name: str, age: int):
-        """Construct a new user."""
+        """
+        Construct a new user.
+
+        Args:
+            name: user's name
+            age: user's age
+        """
         self.name = name
         self.age = age
 
     def get_introduction(self) -> str:
-        """Return a user's self-introduction."""
-        return "Hello, i'm {user.name}, {user.age}".format(user=self)
+        """
+        Return a user's self-introduction.
+
+        Returns:
+            str
+        """
+        return "Hello, i'm {self.name}, {self.age}".format(self=self)
--- tests/test_users.py
+++ tests/test_users.py
@@ -2,23 +2,19 @@
 
 """Users representation tests."""
 
-import unittest
-
 from hexlet_python_package.user import User
 
 
-class TestUser(unittest.TestCase):
-    """Tests the User class routines."""
+def test_instantiation():
+    """Check that User instance has the particular properties."""
+    bob = User('Bob', 42)
+    assert bob.name == 'Bob'
+    assert bob.age == 42
 
-    def test_instantiation(self):
-        """Check that User instance has the particular properties."""
-        bob = User('Bob', 42)
-        self.assertEqual(bob.name, 'Bob')
-        self.assertEqual(bob.age, 42)
 
-    def test_introduction(self):
-        """Check that user introduses herself properly."""
-        alice = User('Alice', 21)
-        intro = alice.get_introduction()
-        self.assertIn('Alice', intro)
-        self.assertIn(str(21), intro)
+def test_introduction():
+    """Check that user introduses herself properly."""
+    alice = User('Alice', 21)
+    intro = alice.get_introduction()
+    assert alice.name in intro
+    assert str(alice.age) in intro
