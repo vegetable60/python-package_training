dc85a72e914b0dc1071f9b29defe1066831697dd
AABur <aabur@mail.ru>
2020-11-06 13:57:43 +0200

    [#4] ci: changed magical numbers ignore
    It is not good to off checking for magical numbers globally, but it's possible locally

--- hexlet_python_package/scripts/hexlet_python_package.py
+++ hexlet_python_package/scripts/hexlet_python_package.py
@@ -7,7 +7,8 @@ from hexlet_python_package import user
 
 def main():
     """Run an example code."""
-    print(user.User(name='Bob', age=42).get_introduction())
+    # it is ok to have some magical numbers locally
+    print(user.User(name='Bob', age=42).get_introduction()) # noqa:WPS432
 
 
 if __name__ == '__main__':
