297a52f5d51741ed32a2952a14badc585011cce5
AABur <aabur@mail.ru>
2020-11-06 14:03:26 +0200

    typo

--- hexlet_python_package/scripts/hexlet_python_package.py
+++ hexlet_python_package/scripts/hexlet_python_package.py
@@ -8,7 +8,7 @@ from hexlet_python_package import user
 def main():
     """Run an example code."""
     # it is ok to have some magical numbers locally
-    print(user.User(name='Bob', age=42).get_introduction()) # noqa:WPS432
+    print(user.User(name='Bob', age=42).get_introduction())  # noqa:WPS432
 
 
 if __name__ == '__main__':
