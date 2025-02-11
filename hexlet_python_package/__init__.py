07ab86cbb12c718357ef7a8413259274740d94a6
Valery Mruz <vegetable060@gmail.com>
2025-02-11 14:40:46 +0300

    organaze imports __init__.py

--- hexlet_python_package/__init__.py
+++ hexlet_python_package/__init__.py
@@ -1,4 +1,4 @@
-import hexlet_python_package.scripts.main as my_lib
 import hexlet_python_package.half as my_half
+import hexlet_python_package.scripts.main as my_lib
 
 __all__ = ('my_lib', 'my_half')
